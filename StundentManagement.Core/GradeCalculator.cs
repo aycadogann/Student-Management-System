dea9d6fd4f6df04a02f7ccd2d3ed90417b62a562
Ayca Dogan <dogan.aycaa@gmail.com>
2026-02-17 21:05:58 +0300

    Add project files.

---
+++ StundentManagement.Core/GradeCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StundentManagement.Core
+{
+    public class GradeCalculator
+    {
+        private const double vizeEtkisi= 0.4;
+        private const double finalEtkisi= 0.4;
+
+        public static double OrtalamaHesapla(double vize, double final)
+        {
+            return (vizeEtkisi*vizeEtkisi)+(final * finalEtkisi);
+        }
+
+        public string HarfNotuGetir(double ortalama)
+        {
+            if (ortalama >= 90) return "AA";
+            if (ortalama >= 85) return "BA";
+            if (ortalama >= 80) return "BB";
+            if (ortalama >= 80) return "BB";
+            if (ortalama >= 80) return "BB";
+            if (ortalama >= 75) return "CB";
+            if (ortalama >= 70) return "CC";
+            if (ortalama >= 60) return "DC";
+            if (ortalama >= 50) return "DD";
+
+            return "FF";
+        }
+
+        public static bool GectiMi(double ortalama)
+        {
+            return ortalama >= 50 ? true : false;
+        }
+
+    }
+}
---
+++ StundentManagement.Core/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StundentManagement.Core
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public double MidTermGrade { get; set; }
+        public double FinalGrade { get; set; }
+    }
+}
