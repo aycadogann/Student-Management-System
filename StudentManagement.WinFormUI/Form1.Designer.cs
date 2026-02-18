489cbef5904e0c433d727816d82c8b04352183fb
Ayca Dogan <dogan.aycaa@gmail.com>
2026-02-18 16:44:53 +0300

    Hata kontrolu yapildi

--- StudentManagement.WinFormUI/Form1.Designer.cs
+++ StudentManagement.WinFormUI/Form1.Designer.cs
@@ -31,29 +31,29 @@ namespace StudentManagement.WinFormUI
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txt_FirstName = new System.Windows.Forms.TextBox();
-            this.txt_LastName = new System.Windows.Forms.TextBox();
-            this.num_MidTerm = new System.Windows.Forms.NumericUpDown();
-            this.num_Final = new System.Windows.Forms.NumericUpDown();
             this.btn_Calculate = new System.Windows.Forms.Button();
+            this.num_Final = new System.Windows.Forms.NumericUpDown();
+            this.num_MidTerm = new System.Windows.Forms.NumericUpDown();
+            this.txt_LastName = new System.Windows.Forms.TextBox();
+            this.txt_FirstName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.dgw_List = new System.Windows.Forms.DataGridView();
-            this.label5 = new System.Windows.Forms.Label();
-            this.lbl_Average = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.lbl_LetterGrade = new System.Windows.Forms.Label();
             this.lbl_Status = new System.Windows.Forms.Label();
+            this.lbl_LetterGrade = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbl_Average = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgw_List = new System.Windows.Forms.DataGridView();
             this.FisrtName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Avarege = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LetterGrate = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.num_MidTerm)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.num_Final)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_MidTerm)).BeginInit();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgw_List)).BeginInit();
             this.SuspendLayout();
@@ -76,35 +76,48 @@ namespace StudentManagement.WinFormUI
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Giriş Paneli";
             // 
-            // label1
+            // btn_Calculate
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(74, 34);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Ad:";
+            this.btn_Calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_Calculate.Location = new System.Drawing.Point(122, 176);
+            this.btn_Calculate.Name = "btn_Calculate";
+            this.btn_Calculate.Size = new System.Drawing.Size(189, 29);
+            this.btn_Calculate.TabIndex = 4;
+            this.btn_Calculate.Text = "Hesapla";
+            this.btn_Calculate.UseVisualStyleBackColor = true;
+            this.btn_Calculate.Click += new System.EventHandler(this.btn_Calculate_Click);
             // 
-            // label2
+            // num_Final
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(48, 67);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Soyad:";
+            this.num_Final.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.num_Final.Location = new System.Drawing.Point(122, 143);
+            this.num_Final.Name = "num_Final";
+            this.num_Final.Size = new System.Drawing.Size(76, 26);
+            this.num_Final.TabIndex = 3;
             // 
-            // label3
+            // num_MidTerm
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(21, 107);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Vize Notu:";
+            this.num_MidTerm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.num_MidTerm.Location = new System.Drawing.Point(122, 105);
+            this.num_MidTerm.Name = "num_MidTerm";
+            this.num_MidTerm.Size = new System.Drawing.Size(76, 26);
+            this.num_MidTerm.TabIndex = 2;
+            // 
+            // txt_LastName
+            // 
+            this.txt_LastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_LastName.Location = new System.Drawing.Point(122, 67);
+            this.txt_LastName.Name = "txt_LastName";
+            this.txt_LastName.Size = new System.Drawing.Size(189, 26);
+            this.txt_LastName.TabIndex = 1;
+            // 
+            // txt_FirstName
+            // 
+            this.txt_FirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_FirstName.Location = new System.Drawing.Point(122, 31);
+            this.txt_FirstName.Name = "txt_FirstName";
+            this.txt_FirstName.Size = new System.Drawing.Size(189, 26);
+            this.txt_FirstName.TabIndex = 0;
             // 
             // label4
             // 
@@ -116,48 +129,35 @@ namespace StudentManagement.WinFormUI
             this.label4.TabIndex = 3;
             this.label4.Text = "Final Notu:";
             // 
-            // txt_FirstName
+            // label3
             // 
-            this.txt_FirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.txt_FirstName.Location = new System.Drawing.Point(122, 31);
-            this.txt_FirstName.Name = "txt_FirstName";
-            this.txt_FirstName.Size = new System.Drawing.Size(189, 26);
-            this.txt_FirstName.TabIndex = 0;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(21, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Vize Notu:";
             // 
-            // txt_LastName
+            // label2
             // 
-            this.txt_LastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.txt_LastName.Location = new System.Drawing.Point(122, 67);
-            this.txt_LastName.Name = "txt_LastName";
-            this.txt_LastName.Size = new System.Drawing.Size(189, 26);
-            this.txt_LastName.TabIndex = 1;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(48, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyad:";
             // 
-            // num_MidTerm
+            // label1
             // 
-            this.num_MidTerm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.num_MidTerm.Location = new System.Drawing.Point(122, 105);
-            this.num_MidTerm.Name = "num_MidTerm";
-            this.num_MidTerm.Size = new System.Drawing.Size(76, 26);
-            this.num_MidTerm.TabIndex = 2;
-            // 
-            // num_Final
-            // 
-            this.num_Final.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.num_Final.Location = new System.Drawing.Point(122, 143);
-            this.num_Final.Name = "num_Final";
-            this.num_Final.Size = new System.Drawing.Size(76, 26);
-            this.num_Final.TabIndex = 3;
-            // 
-            // btn_Calculate
-            // 
-            this.btn_Calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn_Calculate.Location = new System.Drawing.Point(122, 176);
-            this.btn_Calculate.Name = "btn_Calculate";
-            this.btn_Calculate.Size = new System.Drawing.Size(189, 29);
-            this.btn_Calculate.TabIndex = 4;
-            this.btn_Calculate.Text = "Hesapla";
-            this.btn_Calculate.UseVisualStyleBackColor = true;
-            this.btn_Calculate.Click += new System.EventHandler(this.btn_Calculate_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(74, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad:";
             // 
             // groupBox2
             // 
@@ -172,6 +172,53 @@ namespace StudentManagement.WinFormUI
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Sonuç Paneli";
+            // 
+            // lbl_Status
+            // 
+            this.lbl_Status.AutoSize = true;
+            this.lbl_Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_Status.Location = new System.Drawing.Point(117, 152);
+            this.lbl_Status.Name = "lbl_Status";
+            this.lbl_Status.Size = new System.Drawing.Size(0, 20);
+            this.lbl_Status.TabIndex = 4;
+            // 
+            // lbl_LetterGrade
+            // 
+            this.lbl_LetterGrade.AutoSize = true;
+            this.lbl_LetterGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_LetterGrade.Location = new System.Drawing.Point(139, 101);
+            this.lbl_LetterGrade.Name = "lbl_LetterGrade";
+            this.lbl_LetterGrade.Size = new System.Drawing.Size(0, 29);
+            this.lbl_LetterGrade.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(52, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 20);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Harf Notu:";
+            // 
+            // lbl_Average
+            // 
+            this.lbl_Average.AutoSize = true;
+            this.lbl_Average.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_Average.Location = new System.Drawing.Point(139, 61);
+            this.lbl_Average.Name = "lbl_Average";
+            this.lbl_Average.Size = new System.Drawing.Size(0, 29);
+            this.lbl_Average.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(56, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Ortalama:";
             // 
             // dgw_List
             // 
@@ -188,53 +235,6 @@ namespace StudentManagement.WinFormUI
             this.dgw_List.RowTemplate.Height = 24;
             this.dgw_List.Size = new System.Drawing.Size(672, 178);
             this.dgw_List.TabIndex = 2;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(56, 67);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(83, 20);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Ortalama:";
-            // 
-            // lbl_Average
-            // 
-            this.lbl_Average.AutoSize = true;
-            this.lbl_Average.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lbl_Average.Location = new System.Drawing.Point(139, 61);
-            this.lbl_Average.Name = "lbl_Average";
-            this.lbl_Average.Size = new System.Drawing.Size(0, 29);
-            this.lbl_Average.TabIndex = 1;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label7.Location = new System.Drawing.Point(52, 107);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(87, 20);
-            this.label7.TabIndex = 2;
-            this.label7.Text = "Harf Notu:";
-            // 
-            // lbl_LetterGrade
-            // 
-            this.lbl_LetterGrade.AutoSize = true;
-            this.lbl_LetterGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lbl_LetterGrade.Location = new System.Drawing.Point(139, 101);
-            this.lbl_LetterGrade.Name = "lbl_LetterGrade";
-            this.lbl_LetterGrade.Size = new System.Drawing.Size(0, 29);
-            this.lbl_LetterGrade.TabIndex = 3;
-            // 
-            // lbl_Status
-            // 
-            this.lbl_Status.AutoSize = true;
-            this.lbl_Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lbl_Status.Location = new System.Drawing.Point(117, 152);
-            this.lbl_Status.Name = "lbl_Status";
-            this.lbl_Status.Size = new System.Drawing.Size(0, 20);
-            this.lbl_Status.TabIndex = 4;
             // 
             // FisrtName
             // 
@@ -274,8 +274,8 @@ namespace StudentManagement.WinFormUI
             this.Text = "Öğrenci Not Hesaplama Sistemi";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.num_MidTerm)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.num_Final)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_MidTerm)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgw_List)).EndInit();
--- StudentManagement.WinFormUI/Form1.cs
+++ StudentManagement.WinFormUI/Form1.cs
@@ -20,33 +20,52 @@ namespace StudentManagement.WinFormUI
 
         private void btn_Calculate_Click(object sender, EventArgs e)
         {
-            Student student = new Student();
-            student.FirstName = txt_FirstName.Text;
-            student.LastName = txt_LastName.Text;
-            student.MidTermGrade = Convert.ToDouble(num_MidTerm.Value);
-            student.FinalGrade = Convert.ToDouble(num_Final.Value);
-            double average = GradeCalculator.OrtalamaHesapla(student.MidTermGrade, student.FinalGrade);
-            string letterGrade = GradeCalculator.HarfNotuGetir(average);
-            bool status = GradeCalculator.GectiMi(average);
+            try
+            {
+                if (string.IsNullOrEmpty(txt_FirstName.Text) || string.IsNullOrEmpty(txt_LastName.Text))
+                {
+                    throw new Exception("İsim alanları boş bırakılamaz!");
+                }
+                if (num_MidTerm.Value == 0 && num_Final.Value == 0)
+                {
+                    throw new Exception("Lütfen notları girdiğinizden emin olun!");
 
-            lbl_Average.Text = average.ToString();
-            lbl_LetterGrade.Text = letterGrade.ToString();
-            if (status)
-            {
-                lbl_Status.Text = "Geçti";
-                lbl_Status.ForeColor = Color.Green;
+                }
+
+                Student student = new Student();
+                student.FirstName = txt_FirstName.Text;
+                student.LastName = txt_LastName.Text;
+                student.MidTermGrade = Convert.ToDouble(num_MidTerm.Value);
+                student.FinalGrade = Convert.ToDouble(num_Final.Value);
+                double average = GradeCalculator.OrtalamaHesapla(student.MidTermGrade, student.FinalGrade);
+                string letterGrade = GradeCalculator.HarfNotuGetir(average);
+                bool status = GradeCalculator.GectiMi(average);
+               
+                lbl_Average.Text = average.ToString();
+                lbl_LetterGrade.Text = letterGrade.ToString();
+                if (status)
+                {
+                    lbl_Status.Text = "Geçti";
+                    lbl_Status.ForeColor = Color.Green;
+                }
+                else
+                {
+                    lbl_Status.Text = "Kaldı";
+                    lbl_Status.ForeColor = Color.Red;
+                }
+                dgw_List.Rows.Add(student.FirstName, student.LastName, average, letterGrade);
+
+                txt_FirstName.Clear();
+                txt_LastName.Clear();
+                num_MidTerm.Value = 0;
+                num_Final.Value = 0;
+                txt_FirstName.Focus();
             }
-            else
+            catch (Exception ex)
             {
-                lbl_Status.Text = "Kaldı";
-                lbl_Status.ForeColor = Color.Red;
+                MessageBox.Show(ex.Message,"HATA",MessageBoxButtons.OK,MessageBoxIcon.Warning);
             }
-            dgw_List.Rows.Add(student.FirstName, student.LastName, average, letterGrade);
-            txt_FirstName.Clear();
-            txt_LastName.Clear();
-            num_MidTerm.Value = 0;
-            num_Final.Value = 0;
-            txt_FirstName.Focus();
+
         }
     }
 }
